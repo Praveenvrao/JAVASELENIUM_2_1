1048bf6f086405ed812c1e09821597a57587be30
Praveenvrao <vpraveenrao453@gmail.com>
2025-11-05 02:15:58 +0530

    Package name changed DAY12 to DAY 15 as per plan

--- src/test/java/DAY15/Program_1.java
+++ src/test/java/DAY15/Program_1.java
@@ -1,11 +1,11 @@
-package DAY12;
+package DAY15;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Program_1 {
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		WebDriver driver = new ChromeDriver();
@@ -17,7 +17,7 @@ public class Program_1 {
 		}else {
 			System.out.println("Testcase failed and the Actual title is ---->  "+ acttitle);
 		}
-		Thread.sleep(3000);
+		//Thread.sleep(3000);
 		driver.close();
 		
 
