b3d4498714bd9f15588b8edd09f8642bb9de6eca
Praveenvrao <vpraveenrao453@gmail.com>
2025-11-07 17:23:31 +0530

    DAY17_2

--- src/test/java/DAY17/CSSSELECTORS.java
+++ src/test/java/DAY17/CSSSELECTORS.java
@@ -1,7 +1,10 @@
 package DAY17;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import net.bytebuddy.implementation.bytecode.Division;
@@ -16,7 +19,17 @@ public class CSSSELECTORS {
 		driver.manage().window().maximize();
 		
 		//Tag.classname
-		driver.findElement(By.cssSelector("svg#navigation-shopping-cart-ico")).click();
+		//driver.findElement(By.cssSelector("svg#navigation-shopping-cart-ico")).click();
+		
+		//tag#id
+		driver.get("https:www.amazon.com");
+		List<WebElement> l1 = driver.findElements(By.cssSelector("header#navbar-main"));
+		System.out.println(l1);
+		
+		//tag [Attribute]
+		driver.findElement(By.cssSelector("i['hm-icon nav-sprite']")).click();
+		
+		
 
 	}
 
