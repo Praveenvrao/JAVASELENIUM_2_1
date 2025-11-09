ba06b82b6a86784fd7872f81bf2e46cde9fbb8a2
Praveenvrao <vpraveenrao453@gmail.com>
2025-11-10 02:06:13 +0530

    Day17_XPATHAxes_update3

---
+++ src/test/java/DAY17/Xpath_Axes.java
@@ -0,0 +1,28 @@
+package DAY17;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Xpath_Axes {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.amazon.com.au/");
+		driver.manage().window().maximize();
+		
+		//descendant
+		driver.findElement(By.xpath("//div[@id='nav-main']/descendant::a[1]")).click();
+		//Preceding 
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("//a[text() = 'New Releases' and @href='/gp/new-releases/?ref_=nav_em_cs_newreleases_0_1_1_3' ]/preceding::li[1]")).click();
+		Thread.sleep(2000);
+		//following
+		driver.findElement(By.xpath("//a[@href ='/gp/bestsellers/electronics/ref=zg_bs_nav_electronics_0']/following::li/a[text()='Garden']")).click();
+		Thread.sleep(5000);
+		driver.close();
+	}
+
+}
