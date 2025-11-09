33753540b478418a62c86781772733dff6ab2a9a
Praveenvrao <vpraveenrao453@gmail.com>
2025-11-10 02:29:49 +0530

    DAY17_XPATHAxes_update5

--- src/test/java/DAY17/Xpath_Axes.java
+++ src/test/java/DAY17/Xpath_Axes.java
@@ -1,7 +1,10 @@
 package DAY17;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Xpath_Axes {
@@ -24,7 +27,13 @@ public class Xpath_Axes {
 		Thread.sleep(2000);
 		//child
 		driver.findElement(By.xpath("//ul[@class ='nav-ul']/child::li[1]")).click();
-		Thread.sleep(2000);
+		Thread.sleep(3000);
+		//parent
+		driver.findElement(By.xpath("//a[@href='/super?ref_=nav_cs_DiscoBar_amazonbasket']/parent::div")).click();
+		Thread.sleep(5000);
+		List<WebElement> Text = driver.findElements(By.xpath("//span[normalize-space()='Prime member exclusive']"));
+		System.out.println(Text);
+		driver.getTitle();
 		driver.close();
 		
 	}
