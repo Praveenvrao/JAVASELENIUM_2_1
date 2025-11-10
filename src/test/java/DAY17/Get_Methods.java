bf8df173c0d55ef027d0f3f6c2f07681903d29af
Praveenvrao <vpraveenrao453@gmail.com>
2025-11-10 15:05:43 +0530

    DAY17_Get methods & Browser methods

---
+++ src/test/java/DAY17/Get_Methods.java
@@ -0,0 +1,35 @@
+package DAY17;
+
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Get_Methods {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.facebook.com/login.php/");
+		driver.getCurrentUrl();
+		System.out.println(driver.getTitle());
+		driver.getWindowHandle();
+		driver.findElement(By.xpath("//a[normalize-space()='Forgotten account?']")).click();
+		
+		Thread.sleep(4000);
+		WebElement FP1 = driver.findElement(By.xpath("//a[normalize-space()='Forgotten account?']"));
+		FP1.getText();
+		FP1.click();
+		Set<String> WHS = driver.getWindowHandles();
+		System.out.println("Window handles are -> "+ WHS);
+		
+		
+		
+
+	}
+
+}
