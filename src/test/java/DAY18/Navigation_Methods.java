a0432cc2ddee87e2e772cc47219150148d3b308b
Praveenvrao <vpraveenrao453@gmail.com>
2025-11-12 13:16:47 +0530

    DAY18_Browser_Navigational_methods_1

---
+++ src/test/java/DAY18/Navigation_Methods.java
@@ -0,0 +1,32 @@
+package DAY18;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Navigation_Methods {
+
+	public static void main(String[] args) throws Exception {
+		// Browser Navigation methods
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		//driver.navigate().to("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		URL OrngehrmUrl = new URL("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		driver.navigate().to(OrngehrmUrl);
+		
+		driver.navigate().to("https://www.amazon.com.au/");
+		driver.navigate().back();
+		System.out.println(driver.getTitle());
+		driver.navigate().forward();
+		System.out.println(driver.getTitle());
+		
+		
+
+	}
+
+}
