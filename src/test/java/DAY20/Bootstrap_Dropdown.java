48760bd9e59c4bafb0674fdab91686c72925a23e
Praveenvrao <vpraveenrao453@gmail.com>
2025-11-19 17:09:29 +0530

    DAY20_Bootstrapdropdown_2

--- src/test/java/DAY20/Bootstrap_Dropdown.java
+++ src/test/java/DAY20/Bootstrap_Dropdown.java
@@ -1,7 +1,7 @@
 package DAY20;
 
 import java.time.Duration;
-import java.util.List;
+import java.util.List;import javax.naming.spi.ObjectFactoryBuilder;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -20,7 +20,22 @@ public class Bootstrap_Dropdown {
 		driver.findElement(By.xpath("//*[@id='dropdown-toggle']")).click();
 		
 //		selecting single option
-		driver.findElement(By.xpath("//label[text()='Cherry']")).click();
+		//driver.findElement(By.xpath("//label[text()='Cherry']")).click();
+		
+//		Selecting multiple options
+		List<WebElement> Alloptions1 = driver.findElements(By.xpath("//ul[@id='dropdown-menu']//li"));
+
+		for(WebElement OP: Alloptions1) {
+			if(OP.getText().equals("Apple") || OP.getText().equals("Cherry") || OP.getText().equals("Fig")) {
+				OP.click();
+				System.out.println(OP.getText());
+				
+			}
+		}
+		driver.findElement(By.xpath("//*[@id='dropdown-toggle']")).click();
+		String SItems= driver.findElement(By.xpath("//*[@id='selected-items']")).getText();
+		System.out.println("Selected items are > "+ SItems);
+		driver.quit();
 	}
 
 }
