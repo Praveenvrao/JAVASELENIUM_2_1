869957517adb5f051aeccd2ff918aed265359a30
Praveenvrao <vpraveenrao453@gmail.com>
2025-11-29 14:04:01 +0530

    DAY20_Dynmictableassignment_update1

---
+++ src/test/java/DAY20/Dynamictable_assignment.java
@@ -0,0 +1,33 @@
+package DAY20;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class Dynamictable_assignment {
+
+	public static void main(String[] args) {
+		// Dynamic table assignment
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://blazedemo.com/");
+		WebElement DEPRTURE= driver.findElement(By.xpath("//select[@name='fromPort']"));
+		Select depSelect = new Select(DEPRTURE);
+		System.out.println(depSelect.getOptions());
+		depSelect.selectByVisibleText("Mexico City");
+		WebElement DESTINY = driver.findElement(By.xpath("//select[@name='toPort']"));
+		Select desSelect = new Select(DESTINY);
+		System.out.println(desSelect.getOptions());
+		desSelect.selectByVisibleText("New York");
+		driver.findElement(By.xpath("//input[@value ='Find Flights']")).click();
+		
+
+	}
+
+}
