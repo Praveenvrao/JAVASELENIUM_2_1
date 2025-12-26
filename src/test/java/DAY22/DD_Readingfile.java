7aa33bbc9bfe8947c210be5d44baa6f089c3e6b1
Praveenvrao <vpraveenrao453@gmail.com>
2025-12-27 01:10:24 +0530

    DAY22_Testdata reading1

---
+++ src/test/java/DAY22/DD_Readingfile.java
@@ -0,0 +1,20 @@
+package DAY22;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DD_Readingfile {
+
+	public static void main(String[] args) throws Exception {
+		// Reading the excel file
+		
+		WebDriver driver = new ChromeDriver();
+		FileInputStream File1 = new FileInputStream("");
+		
+
+	}
+
+}
