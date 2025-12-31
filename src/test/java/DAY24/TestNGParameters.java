a6c7b9c6591c0417829d511893b461c6000017ea
Praveenvrao <vpraveenrao453@gmail.com>
2025-12-31 06:46:02 +0530

    DAY24_TestNG_Groups

--- src/test/java/DAY24/TestNGParameters.java
+++ src/test/java/DAY24/TestNGParameters.java
@@ -5,20 +5,20 @@ import org.testng.annotations.Test;
 
 public class TestNGParameters {
 	
-	@Test (priority =1, groups = {"APP","PAYMENTS"})
+	@Test (priority =1, groups = {"APP"})
 	public void Login() {
 		System.out.println("This is Login functionality");
 		Assert.assertTrue(1==1);
 	}
-	@Test (priority = 2, groups = {"PAYMENTS"}, dependsOnMethods = {"Login"})
+	@Test (priority = 2, groups = {"APP", "PAYMENTS", "PAY"}, dependsOnMethods = {"Login"})
 	public void SearchPayment() {
 		System.out.println("This is Search Payment functionality");
 	}
-	@Test (priority = 3, groups = {"PAYMENTS"}, dependsOnMethods = {"Login"} )
+	@Test (priority = 3, groups = {"APP","PAYMENTS", "PAY"}, dependsOnMethods = {"Login"} )
 	public void Paymentstatus() {
 		System.out.println("Checking Payment status functionality");
 	}
-	@Test (priority = 4, groups = {"APP", "PAYMENTS"}, dependsOnMethods = {"Login"})
+	@Test (priority = 4, groups = {"APP"}, dependsOnMethods = {"Login"})
 	public void Logout() {
 		System.out.println("This is Logout functionality");
 	}
--- src/test/java/DAY24/TestNGParameters2.java
+++ src/test/java/DAY24/TestNGParameters2.java
@@ -5,16 +5,16 @@ import org.testng.annotations.Test;
 
 public class TestNGParameters2 {
 	
-	@Test (priority =1 , groups = {"APP", "ADMIN"})
+	@Test (priority =1 , groups = {"APP"})
 	void LoginApp(){
 		System.out.println("This is ADMIN Login");
 		Assert.assertTrue(true);
 	}
-	@Test(priority =2, dependsOnMethods = {"LoginApp"}, groups = {"APP", "ADMIN"})
+	@Test(priority =2, dependsOnMethods = {"LoginApp"}, groups = {"APP", "ADMIN", "OCA"})
 	void SearchAc() {
 		System.out.println("This is ADMIN Search");
 	}
-	@Test (priority =3, dependsOnMethods = {"LoginApp"}, groups = {"APP", "ADMIN"})
+	@Test (priority =3, dependsOnMethods = {"LoginApp"}, groups = {"APP", "ADMIN", "OCA"})
 	void SearchACdetails() {
 		System.out.println("This is method for searching account details");
 	}
